56c9a2e31306d373b7d06e216db1ba7fcf5ea4c3
Marek Zdybel <marek.zdybel@sbg.com.pl>
2020-11-08 15:55:48 +0100

    VUEjs - Component life cycle - Error catch II

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="bg-primary text-white m-2 p-2">
+  <div class="bg-danger text-white text-center h3 p-2" v-if="error.occurred">
+    Wystąpił błąd
+    <h4>
+      Błąd: "{{ error.error }}" ({{ error.source }})
+    </h4>
+  </div>
+  <div v-else class="bg-primary text-white m-2 p-2">
     <div class="form-check">
       <input class="form-check-input" type="checkbox" v-model="checked" />
       <label>Pole checkbox</label>
@@ -8,64 +14,30 @@
     <div class="bg-info p-2" v-if="checked">
       <message-display></message-display>
     </div>
-    <div class="bg-info p-2">
-      Imiona:
-      <ul>
-        <li v-for="name in names" v-bind:key="name">
-          {{ name }}
-        </li>
-      </ul>
-    </div>
-    <div class="text-white center my-2">
-      <button class="btn btn-light" v-on:click="doChange">
-        Zmień
-      </button>
-    </div>
   </div>
 </template>
 
 <script>
 import MessageDisplay from "./components/MessageDisplay"
-import Vue from "vue";
 export default {
   name: 'App',
   components: { MessageDisplay },
   data: function () {
     return {
       checked: true,
-      names: []
+      names: [],
+      error: {
+        occurred: false,
+        error: "",
+        source: ""
+      }
     }
   },
-  beforeCreate() {
-    console.log("Wywołanie metody beforeCreate " + this.checked);
-  },
-  created() {
-    console.log("Wywołanie metody created " + this.checked);
-  },
-  mounted() {
-    this.$el.dataset.names.split(",").forEach(name => this.names.push(name));
-  },
-  beforeUpdate() {
-    console.log(`Wywołano metodę beforeUpdate. Stan zaznaczenia: ${this.checked}`
-      + ` Imię: ${this.names[0]} Element listy: `
-      + this.$el.getElementsByTagName("li").length);
-  },
-  updated() {
-    console.log(`Wywołano metodę updated. Stan zaznaczenia: ${this.checked}`
-      + ` Imię: ${this.names[0]} Element listy: `
-      + this.$el.getElementsByTagName("li").length);
-  },
-  methods: {
-    doChange() {
-      this.checked = !this.checked;
-      this.names.reverse();
-      Vue.nextTick(() => console.log('Wykonano wywołanie zwrotne'));
-    }
-  },
-  watch: {
-    checked: function (newValue, oldValue) {
-      console.log(`Sprawdzono obserwatora, Stara wartość: ${oldValue}, Nowa wartość: ${newValue}`);
-    }
+  errorCaptured(error, component, source) {
+    this.error.occurred = true;
+    this.error.error = error;
+    this.error.source = source;
+    return false;
   }
 }
 </script>
