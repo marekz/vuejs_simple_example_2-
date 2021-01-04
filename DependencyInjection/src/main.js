c742410611c766530caafe189709d3eb01880caf
Marek Zdybel <marek@motta.com.pl>
2021-01-04 21:09:30 +0100

    Create HTTP service

--- DependencyInjection/src/main.js
+++ DependencyInjection/src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
+import { RestDataSource } from "./restDataSource";
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
   provide: function () {
     return {
-      eventBus: new Vue()
+      eventBus: new Vue(),
+      restoreDataSource: new RestDataSource()
     }
   }
 }).$mount('#app')
