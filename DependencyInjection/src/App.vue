ce27f644318b776d7c78d45df158635583f6ca83
Marek Zdybel <marek@motta.com.pl>
2022-01-10 00:02:07 +0100

    Using routing do component display

--- DependencyInjection/src/App.vue
+++ DependencyInjection/src/App.vue
@@ -1,35 +1,23 @@
 <template>
   <div class="container-fluid">
-    <div class="col">
-      <div class="col text-center m-2">
-        <button class="btn btn-primary"
-                v-on:click="selectComponent('table')">
-          Funkcje standardowe
-        </button>
-        <button class="btn btn-success"
-                v-on:click="selectComponent('summary')">
-          Funkcje zaawansowane
-        </button>
-      </div>
-    </div>
     <div class="row">
-      <div class="col">
-        <component v-bind:is="selectedComponent"></component>
+      <div class="col m-2">
+        <router-view></router-view>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import ProductDisplay from './components/ProductDisplay';
-import ProductEditor from "./components/ProductEditor";
-import ErrorDisplay from "./components/ErrorDisplay";
-import LoadingMessage from "./components/LoadingMessage";
-const DataSummary = () => ({
-  component: import(/* webpackChunkName: "advanced" */"./components/DataSummary"),
-  loading: LoadingMessage,
-  delay: 100
-});
+// import ProductDisplay from './components/ProductDisplay';
+// import ProductEditor from "./components/ProductEditor";
+// import ErrorDisplay from "./components/ErrorDisplay";
+// import LoadingMessage from "./components/LoadingMessage";
+// const DataSummary = () => ({
+//   component: import(/* webpackChunkName: "advanced" */"./components/DataSummary"),
+//   loading: LoadingMessage,
+//   delay: 100
+// });
 
 import {
   mapState,
@@ -38,36 +26,36 @@ import {
 
 export default {
   name: 'App',
-  components: {
-    ProductDisplay,
-    ProductEditor,
-    ErrorDisplay,
-    DataSummary
-  },
+  // components: {
+  //   ProductDisplay,
+  //   ProductEditor,
+  //   ErrorDisplay,
+  //   DataSummary
+  // },
   created() {
     this.$store.dispatch("getProductsAction");
   },
-  methods: {
-    ...mapMutations({
-      selectComponent: "nav/selectComponent"
-    })
-  },
-  computed: {
-    ...mapState({
-      selected: state => state.nav.selected
-    }),
-    selectedComponent() {
-      switch (this.selected) {
-        case "table":
-          return ProductDisplay;
-        case "editor":
-          return ProductEditor;
-        case "summary":
-          return DataSummary;
-      }
-
-      return ProductDisplay;
-    }
-  }
+  // methods: {
+  //   ...mapMutations({
+  //     selectComponent: "nav/selectComponent"
+  //   })
+  // },
+  // computed: {
+  //   ...mapState({
+  //     selected: state => state.nav.selected
+  //   }),
+  //   selectedComponent() {
+  //     switch (this.selected) {
+  //       case "table":
+  //         return ProductDisplay;
+  //       case "editor":
+  //         return ProductEditor;
+  //       case "summary":
+  //         return DataSummary;
+  //     }
+  //
+  //     return ProductDisplay;
+  //   }
+  // }
 }
 </script>
