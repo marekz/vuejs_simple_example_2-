58ca992c7d7755b6fa3bff3c89531be24a62bef3
Marek Zdybel <marek@motta.com.pl>
2022-01-11 06:15:42 +0100

    CatchAll define in route

--- DependencyInjection/src/router/index.js
+++ DependencyInjection/src/router/index.js
@@ -14,6 +14,10 @@ export default new VueRouter({
         {
             path: "/edit",
             component: ProductEditor
+        },
+        {
+            path: "*",
+            redirect: "/"
         }
     ]
 })
