166e4c5df246149441e3ac2b68d33332889c9bb0
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-01-23 17:43:57 +0100

    Create nested routes

---
+++ DependencyInjection/src/components/Preferences.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h4 class="bg-info text-white text-center p-2">Preferencje</h4>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" v-bind:checked="primaryEdit" v-on:input="setPrimaryEdit">
+      <label class="form-check-label">Główny kolor dla przycisków edycji</label>
+    </div>
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" v-bind:checked="dangerDelete" v-on:input="setDangerDelete">
+      <label class="form-check-label">Kolor ostrzeżenia dla przycisków usuwania</label>
+    </div>
+  </div>
+</template>
+
+<script>
+import {
+  mapState
+} from "vuex";
+
+export default {
+  computed: {
+    ...mapState({
+      primaryEdit: state => state.prefs.promaryEditButton,
+      dangerDelete: state => state.prefs.dangerDeleteButton
+    })
+  },
+  methods: {
+    setPrimaryEdit() {
+      this.$score.commit("prefs/setEditButtonColor", !this.primaryEdit);
+    },
+    setDangerDelete() {
+      this.$store.commit("prefs/setDeleteButtonColor", !this.dangerDelete);
+    }
+  }
+}
+</script>
--- DependencyInjection/src/router/index.js
+++ DependencyInjection/src/router/index.js
@@ -2,25 +2,43 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import ProductDisplay from "@/components/ProductDisplay";
 import ProductEditor from "@/components/ProductEditor";
+import Preferences from "@/components/Preferences";
+import Products from "@/components/Products";
 
 Vue.use(VueRouter);
 export default new VueRouter({
     mode: "history",
     routes: [
         {
-            name: "table",
-            path: "/",
-            component: ProductDisplay,
-            alias: "/list"
+            path: "/preferences",
+            component: Preferences
         },
         {
-            name: "editor",
-            path: "/:op(create|edit)/:id(\\d+)?",
-            component: ProductEditor
+            path: "/products",
+            component: Products,
+            children: [{
+                    name: "table",
+                    path: "list",
+                    component: ProductDisplay
+                },
+                {
+                    name: "editor",
+                    path: ":op(create|edit)/:id(\\d+)?",
+                    component: ProductEditor
+                },
+                {
+                    path: "",
+                    redirect: "list"
+                }
+            ]
+        },
+        {
+            path: "/edit/:id",
+            redirect: to => `/products/edit/${to.params.id}`
         },
         {
             path: "*",
-            redirect: "/"
+            redirect: "/products/list"
         }
     ]
 })
