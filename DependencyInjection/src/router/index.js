0a942b8e4a02762835e207932a5354f021b2de9c
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-02-17 07:16:58 +0100

    Using separate files to organize route

--- DependencyInjection/src/router/index.js
+++ DependencyInjection/src/router/index.js
@@ -1,63 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import ProductDisplay from "@/components/ProductDisplay";
-import ProductEditor from "@/components/ProductEditor";
-import Preferences from "@/components/Preferences";
-import Products from "@/components/Products";
-import SideBySide from "@/components/SideBySide";
-
+// import ProductDisplay from "@/components/ProductDisplay";
+// import ProductEditor from "@/components/ProductEditor";
+// import Preferences from "@/components/Preferences";
+// import Products from "@/components/Products";
+// import SideBySide from "@/components/SideBySide";
+import BasicRouters from "@/router/basicRouters";
+import SideBySideRouters from "@/router/sideBySideRouters";
 Vue.use(VueRouter);
 export default new VueRouter({
     mode: "history",
     routes: [
-        {
-            path: "/preferences",
-            component: Preferences
-        },
-        {
-            path: "/products",
-            component: Products,
-            children: [{
-                    name: "table",
-                    path: "list",
-                    component: ProductDisplay
-                },
-                {
-                    name: "editor",
-                    path: ":op(create|edit)/:id(\\d+)?",
-                    component: ProductEditor
-                },
-                {
-                    path: "",
-                    redirect: "list"
-                }
-            ]
-        },
-        {
-            path: "/edit/:id",
-            redirect: to => `/products/edit/${to.params.id}`
-        },
-        {
-            path: "/named",
-            component: SideBySide,
-            children: [{
-                path: "tableleft",
-                components: {
-                    left: ProductDisplay,
-                    right: ProductEditor
-                }
-            },
-            {
-                path: "tableright",
-                components: {
-                    left: ProductEditor,
-                    right: ProductDisplay
-                }
-            }]
-        },
-        {
-            path: "*",
-            redirect: "/products/list"
-        }
+        ...BasicRouters,
+        SideBySideRouters,
     ]
 })
