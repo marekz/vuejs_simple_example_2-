050d7a68ff142a6fb5cd313acbdb17a3a36583e6
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-05-26 20:36:05 +0200

    Switching between multiple items

--- transations/src/components/SimpleDisplay.vue
+++ transations/src/components/SimpleDisplay.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="mx-5 border border-dark p-2">
     <h3 class="bg-warning text-white text-center p-2">Wyświetlacz</h3>
-    <transition enter-to-class="fadeIn" leave-to-class="fadeOut">
-      <div v-if="show" class="animated h4 bg-info text-center p-2">Cześć, Kolo...</div>
+    <transition enter-active-class="fadeIn quick" leave-active-class="fadeOut quick" mode="out-in">
+      <div v-if="show" class="animated h4 bg-info text-center p-2" key="hello">Cześć, Kolo...</div>
+      <div v-else class="animated h4 bg-success text-center p-2" key="goodbye">Nara, Kolo...</div>
     </transition>
     <div class="text-center">
       <button class="btn btn-primary" v-on:click="toggle">
