57789ff8d751a360c941dfca89c0cbd30760f43e
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-05-26 20:18:53 +0200

    Simple transition

--- transations/src/components/SimpleDisplay.vue
+++ transations/src/components/SimpleDisplay.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="mx-5 border border-dark p-2">
     <h3 class="bg-warning text-white text-center p-2">Wyświetlacz</h3>
-    <div v-if="show" class="h4 bg-info text-center p-2">Cześć, Kolo...</div>
+    <transition>
+      <div v-if="show" class="h4 bg-info text-center p-2">Cześć, Kolo...</div>
+    </transition>
     <div class="text-center">
       <button class="btn btn-primary" v-on:click="toggle">
         Przełącz widoczność
@@ -24,3 +26,20 @@ export default {
   }
 }
 </script>
+
+<style>
+  .v-leave-active {
+    opacity: 0;
+    font-size: 0em;
+    transition: all 250ms;
+  }
+  .v-enter {
+    opacity: 0;
+    font-size: 0em;
+  }
+  .v-enter-to {
+    opacity: 1;
+    font-size: x-large;
+    transition: all 250ms;
+  }
+</style>
