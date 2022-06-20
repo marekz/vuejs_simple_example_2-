6d39a274d2852c6c93496c9d382ca33d44e4e718
Marek Zdybel <marek.zdybel@sbg.com.pl>
2022-06-20 19:29:57 +0200

    Create component mixins

--- extendingvue/src/App.vue
+++ extendingvue/src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="m-2">
     <Numbers/>
+    <Subtraction/>
   </div>
 </template>
 
 <script>
 import Numbers from "@/components/Numbers";
+import Subtraction from "@/components/Subtraction";
 export default {
   name: 'App',
   components: {
-    Numbers
+    Numbers, Subtraction
   }
 }
 </script>
--- extendingvue/src/main.js
+++ extendingvue/src/main.js
@@ -5,6 +5,8 @@ import Colorize from "@/directives/colorize";
 Vue.directive("colorize", Colorize);
 
 Vue.config.productionTip = false
+import mixin from "@/mixins/numbersMixins";
+Vue.mixin(mixin);
 
 new Vue({
   render: h => h(App),
